4e495f706fecdb7ba629096c2f411537eb7cb333
ChunChih Lin <chunchi@g.clemson.edu>
2022-03-07 20:20:12 -0500

    Update redis_throughput.py

--- gr-pydatabase/examples/redis-notifications/redis_throughput.py
+++ gr-pydatabase/examples/redis-notifications/redis_throughput.py
@@ -5,16 +5,25 @@ import time
 import json
 import random
 import string
+from time import sleep
+from datetime import datetime
+import sys
 
-Loop_MAX = 1
+Loop_MAX = 1000
 # The maximum MSDU is 1500 bytes before encryption.
-MSDU_MAX = 1000
+MSDU_MAX = 100
+ID = sys.argv[1] or "a"
+
+sleep_bw_pkt = float(sys.argv[2]) or 0.01
 
 r, subprefix = utils.redis_setup(db_host='localhost', db_port=6379, db_ch='channel_1', db_idx=0)
 
 def generate_MSDU(idx):
+	print(f'Generating [{idx+1}/{Loop_MAX}] packet...', end='\r')
 	MSDU = dict()
 	MSDU["idx"] = str(idx)
+	MSDU["total"] = str(Loop_MAX)
+	MSDU["sequence"] = ID
 	MSDU["data"] = ""
 	MSDU["timestamp"] = str(time.time())
 	length_remain = len(json.dumps(MSDU))
@@ -27,7 +36,9 @@ def run_without_pipeline():
 	for x in range(Loop_MAX):
 		set_value = generate_MSDU(x)
 		r.set("Trans:"+str(x), set_value)
+		sleep(sleep_bw_pkt)
 		pass
+
 	print("Total time w/o pipeline: ", time.time()-start_time)
 	pass
 
@@ -42,6 +53,9 @@ def run_with_pipeline():
 
 def main():
 	r.flushall()
+	print("Start transmitting packets...")
+	print(f"Rest interval: {sleep_bw_pkt} seconds.")
+	print(f"Sequence: {ID}, Total packets: {Loop_MAX}")
 	run_without_pipeline()
 	# run_with_pipeline()
 
