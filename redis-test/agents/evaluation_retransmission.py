34a35662c82813df29a18e14355f52eef706a18f
Chun-Chih Lin <chunchi@g.clemson.edu>
2022-08-14 16:30:45 -0400

    Update evaluation_retransmission.py to show the information in database

--- redis-test/agents/evaluation_retransmission.py
+++ redis-test/agents/evaluation_retransmission.py
@@ -2,23 +2,51 @@ import redis as redis
 import time
 import json
 
+from operator import attrgetter
+from collections import namedtuple
+
+ReceivedMsg = namedtuple('ReceivedMsg', ['id', 'total', 'seq', 'timestamp', 'data'])
+
 db = redis.Redis(host='localhost', port=6379, db=0)
 
 def evaluation(db_keys):
-	for i, key in enumerate(db_keys):
-		hash_value = db.hgetall(key)
-		try:
-			payload = json.loads(hash_value[b'payload'])
-			seq = payload['sequence']
-			print(f'key: {key.decode("utf-8")}, seq: {seq}')
-		except Exception as e:
-			print(f'Exception: {e}')
+    received_list = []
+    for _, key in enumerate(db_keys):
+        hash_value = db.hgetall(key)
+        try:
+            payload = json.loads(hash_value[b'payload'])
+
+            seq = payload['sequence'].split('.')[0]
+
+            received = ReceivedMsg(int(payload['idx']), int(payload['total']), 
+                                   seq, float(payload['timestamp']), payload['data'])
+            received_list.append(received)
+        except Exception as e:
+            print(f'Exception: {e}')
+
+    sorted_msg = sorted(received_list, key=attrgetter('timestamp'))
+    return sorted_msg
+
+def display_results(msgs):
+    show_data_len = 30
+
+    str_id, str_ttl, str_seq, str_time, str_data = "ID", "Total", "Sequence", "TimeStamp", "Message"
+    print("==============================================================")
+    print(f'{str_id:>3}/{str_ttl:<10} {str_seq:<10} {str_data:<34}')
+    print("--------------------------------------------------------------")
+    for message in msgs:
+        idx, ttl, seq, timestamp, data = message
+        print(f'{idx+1:>3}/{ttl:<10} {seq:<10} {data[0:show_data_len]}', end='')
+        if len(data) > show_data_len:
+            print('...', end='')
+        print('')
+    pass
 
 def main():
-	all_keys = db.keys('Recv:*')
-	print(f'all_keys, type: {type(all_keys)}')
-	evaluation(all_keys)
-	pass
+    all_keys = db.keys('Recv:*')
+    stored_msg = evaluation(all_keys)
+    display_results(stored_msg)
+    pass
 
 if __name__ == '__main__':
 	main()
