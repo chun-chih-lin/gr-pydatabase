109a8de31cf4033733f7c8786fb7c4d47887d194
Chun-Chih Lin <chunchi@g.clemson.edu>
2023-01-30 14:04:55 -0500

    general update

--- redis-test/CSI_Detect_Interference/detect.py
+++ redis-test/CSI_Detect_Interference/detect.py
@@ -4,11 +4,11 @@ from matplotlib import pyplot as plt
 import sys
 
 def read_np(prefix):
-	with open(f'{prefix}_csi.np', 'rb') as f:
+	with open(f'{prefix}_csi.npy', 'rb') as f:
 		csi = np.load(f)
-	with open(f'{prefix}_snr.np', 'rb') as f:
+	with open(f'{prefix}_snr.npy', 'rb') as f:
 		snr = np.load(f)
-	with open(f'{prefix}_timestamp.np', 'rb') as f:
+	with open(f'{prefix}_timestamp.npy', 'rb') as f:
 		timestamp = np.load(f)
 	return snr, csi, timestamp
 
@@ -44,9 +44,9 @@ def detecting_interference(csi_ary):
 	csi_l = 52
 	avg_len = 2
 
-	# plt.figure()
-	slope_threshold_min = 0.75
-	slope_threshold_max = 1.25
+	plt.figure()
+	slope_threshold_min = 0.85
+	slope_threshold_max = 1.15
 	detection = np.zeros(len(csi_ary))
 	for csi_i, csi in enumerate(csi_ary):
 		slopes = []
@@ -60,8 +60,8 @@ def detecting_interference(csi_ary):
 				slopes.append(slope)
 				if slope_threshold_min > slope or slope > slope_threshold_max and detection[csi_i] == 0:
 					detection[csi_i] = 1
-		# plt.plot(slopes)
-	# plt.grid(True)
+		plt.plot(slopes)
+	plt.grid(True)
 	
 
 	plt.figure()
@@ -80,7 +80,6 @@ def detecting_interference(csi_ary):
 
 
 def process(prefix):
-	print(f'{prefix}_snr.np')
 	snr, csi, timestamp = read_np(prefix)
 
 	detecting_interference(csi)
