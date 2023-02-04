0941282ec1db0413273d6d5a66c1986796fb02a5
Chun-Chih Lin <chunchi@g.clemson.edu>
2023-02-04 13:53:26 -0500

    update action agent

--- redis-test/agents/action_agent.py
+++ redis-test/agents/action_agent.py
@@ -87,6 +87,8 @@ class ActionAgent(object):
 				else:
 					# The key is older somehow. Discard it.
 					self.db.delete(csi_key)
+			elif action == "DEBUG":
+				self.detect_interference()
 			else:
 				print(f"Other action: {aciton}.")
 				
@@ -101,8 +103,69 @@ class ActionAgent(object):
 		return self.utf8_decode(msg['channel']).split(":")[3]
 
 	#--------------------------------------------------------------------------------
+	# For Action: Detecting Interference
+	def get_all_csi_in_db(self):
+		csi_keys_list = self.db.lrange("SYSTEM:CSI:QUEUE", 0, -1)
+		csi_list = []
+		for csi_key in csi_keys_list:
+			csi_json_str = self.db.get(csi_key).decode('utf-8')
+			csi_json = json.loads(csi_json_str)
+			csi = np.array(csi_json['real']) + 1j*np.array(csi_json['imag'])
+			csi_list.append(csi)
+		return np.array(csi_list)
+
+	def sliding_var_detect(self, csis):
+		sliding_detect_csis = np.zeros((csis.shape[0],csis.shape[1]-self.sliding_window_size))
+		for csi_i, csi in enumerate(csis):
+			ret = []
+			for i, s in enumerate(csi[:len(csi)-self.sliding_window_size]):
+				ret.append(np.mean(np.var(csi[i:i+self.sliding_window_size])))
+
+			sliding_detect_csis[csi_i, :] = ret
+		return sliding_detect_csis
+	
+	def median_max_detect(self, sliding_var_ary):
+		median_max_detection = []
+
+		for d_i, d in enumerate(sliding_var_ary):
+			median = np.median(d)
+			max_v = np.max(d)
+			if median == 0:
+				median_max_detection.append(0)
+			elif max_v/median > self.median_threshold:
+				median_max_detection.append(1)
+			else:
+				median_max_detection.append(0)
+		return median_max_detection
+
+	def consecutive_detect(self, detections):
+		consecutive_detection = [a*b for (a, b) in zip(detections[:-1], detections[2:])]
+		return consecutive_detection
+
+
 	def detect_interference(self):
 		print('[Action] Detecting interference...')
+
+		# TODO: Setting should be set in db for dynamic changes
+		self.sliding_window_size = 4
+		self.median_threshold = 5.0
+
+		FAILRED = '\033[91m'
+		OKGREEN = '\033[92m'
+		WARNING = '\033[93m'
+		EOC = '\033[0m'
+		csi_list = self.get_all_csi_in_db()
+
+		sliding_var_detect_csis = self.sliding_var_detect(csi_list)
+		detections = self.median_max_detect(sliding_var_detect_csis)
+		consecutive_detection = self.consecutive_detect(detections)
+		print(f'consecutive_detection: {consecutive_detection}')
+
+		
+
+		
+		
+
 
 
 def main():
